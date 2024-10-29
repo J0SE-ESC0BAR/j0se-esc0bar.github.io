0e7306315942a903edc8d960696178adc4ad9a08
José Alfonso Escobar Mejía <josealfonsoescobarmejia@gmail.com>
2024-10-28 22:18:38 -0600

    Mejoras en vistas de imagenes en posts

--- content/posts/Dotnet/_index.md
+++ content/posts/Dotnet/_index.md
@@ -1,8 +1,8 @@
 ---
 title: ".NET"
 date: 2023-10-13T10:29:46-06:00
-image: images/.NET.png
 draft: false
+image: images/dotnet.png
 viewimg: true
 type: "directorio"
 description: '.NET es una plataforma para desarrolladores formada por herramientas, lenguajes de programación y bibliotecas para crear muchos tipos diferentes de aplicaciones.'
---
+++ docs/js/bundle.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".nav-toggle"),
+  navMenu = document.querySelector(".nav-menu");
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible"),
+    navMenu.classList.contains("nav-menu_visible")
+    ? navToggle.setAttribute("aria-label", "Cerrar menú")
+    : navToggle.setAttribute("aria-label", "Abrir menú");
+});
