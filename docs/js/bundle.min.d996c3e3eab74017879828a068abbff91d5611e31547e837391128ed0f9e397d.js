d56d27c6f9e4c8895fcb597652347f18a3535b21
José Alfonso Escobar Mejía <josealfonsoescobarmejia@gmail.com>
2025-05-24 15:05:11 -0600

    Elimine carpetas de las paginas estaticas

--- docs/js/bundle.min.d996c3e3eab74017879828a068abbff91d5611e31547e837391128ed0f9e397d.js
+++
@@ -1,1 +0,0 @@
-const navToggle=document.querySelector(".nav-toggle"),navMenu=document.querySelector(".nav-menu");navToggle.addEventListener("click",()=>{navMenu.classList.toggle("nav-menu_visible"),navMenu.classList.contains("nav-menu_visible")?navToggle.setAttribute("aria-label","Cerrar menú"):navToggle.setAttribute("aria-label","Abrir menú")})
